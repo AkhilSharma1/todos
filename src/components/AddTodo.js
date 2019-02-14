a7d215764b3999bc66e76c2cbd17638dbbd57a6a
Akhil Sharma <akhil.sharma250@gmail.com>
2019-02-14 13:44:06 +0530

    cleanup

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,40 +1,6 @@
 import React, { useState } from "react";
 
-// export class AddTodo extends Component {
-//   state = {
-//     value: ""
-//   };
 
-//   handleTodoChange = e => {
-//     this.setState({
-//       value: e.target.value
-//     });
-//   };
-
-//   handleSubmit = (e)=>{
-//       e.preventDefault()
-//       if(this.state.value !== '') {
-//         this.props.addTodo(this.state.value);
-//         this.setState({
-//             value: ''
-//         });
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <form onSubmit={this.handleSubmit}>
-//         <input
-//           value={this.state.value}
-//           placeholder=" Add a todo here..."
-//           autoComplete="off"
-//           onChange={this.handleTodoChange}
-//         />
-//         <input type="submit" value="ADD" />
-//       </form>
-//     );
-//   }
-// }
 export const AddTodo = ({ addTodo }) => {
   const [inputValue, setInputValue] = useState("");
 
